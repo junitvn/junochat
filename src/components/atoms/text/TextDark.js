bb4226aa9e71c3891872b22dca5c27ec3d613eb5
lamnn_laptop <lamnn.dev@gmail.com>
2020-10-26 00:09:50 +0700

    Add alias

--- src/components/atoms/text/TextDark.js
+++ src/components/atoms/text/TextDark.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import {View, Text} from 'react-native';
+import {Colors} from '_styles';
 
 export default function TextDark({text, size}) {
+  console.log(Colors.PRIMARY);
   return <Text style={{fontSize: size, color: '#868f96'}}>{text}</Text>;
 }
--- src/components/molecules/buttons/ButtonLoginWith.js
+++ src/components/molecules/buttons/ButtonLoginWith.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import {View, Text} from 'react-native';
 import {TouchableOpacity} from 'react-native-gesture-handler';
 import TextDark from '../../atoms/text/TextDark';
 
